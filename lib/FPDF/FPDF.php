38775031e00eac342db85686eaefea1b52195d20
Anton K <wiistriker@gmail.com>
2015-03-13 13:42:32 +0300

    tfpdf

--- lib/FPDF/FPDF.php
+++ lib/FPDF/FPDF.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once __DIR__.'/src/FPDF/fpdf.php';
+//require_once __DIR__.'/src/FPDF/fpdf.php';
+require_once __DIR__.'/src/fpdf_tfpdf.php';
 
 class FPDF_FPDF extends FPDF
 {
---
+++ lib/FPDF/src/fpdf_tfpdf.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__.'/TFPDF/tfpdf.php';
+
+class FPDF extends tFPDF
+{
+}

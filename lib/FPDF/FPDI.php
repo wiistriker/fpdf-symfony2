9361bab47b1e8a41f65e7c00906379b7541c7f06
Anton K <wiistriker@gmail.com>
2015-03-13 11:54:33 +0300

    tftpd

--- lib/FPDF/FPDI.php
+++ lib/FPDF/FPDI.php
@@ -1,8 +1,8 @@
 <?php
-# vendor/geshi/lib/Geshi/Geshi.php
 
 require_once __DIR__.'/src/fpdi.php';
 
-class FPDF_FPDI extends FPDI {
+class FPDF_FPDI extends FPDI
+{
 
 }
---
+++ lib/FPDF/TFPDF.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__.'/src/tfpdf.php';
+
+class TFPDF_FPDF extends tFPDF
+{
+
+}
